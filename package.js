1fee826de56a269f65f676002d4c8c5a8c4e8f8f
StorytellerCZ <storyteller@freedombase.net>
2018-01-25 13:13:02 +0100

    Update package.js

--- package.js
+++ package.js
@@ -1,23 +1,21 @@
 Package.describe({
-    name: "socialize:follow",
-    summary: "A social follow package",
-    version: "0.1.0",
-    git: "https://github.com/copleykj/socialize-follow.git"
+  name: 'socialize:follow',
+  summary: 'A social follow package',
+  version: '0.1.0',
+  git: 'https://github.com/copleykj/socialize-follow.git'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom("1.0.2.1");
+  api.versionsFrom('1.3');
 
-    api.use([
-        "socialize:user-model@0.1.7"
-    ]);
+  api.use(['socialize:user-model@1.0.0']);
 
-    api.imply("socialize:user-model");
+  api.imply('socialize:user-model');
 
-    //Add the follow-model files
-    api.addFiles("follow-model/common/follow-model.js");
-    api.addFiles("follow-model/common/user-extensions.js");
-    api.addFiles("follow-model/server/server.js", "server");
+  //Add the follow-model files
+  api.addFiles('follow-model/common/follow-model.js');
+  api.addFiles('follow-model/common/user-extensions.js');
+  api.addFiles('follow-model/server/server.js', 'server');
 
-    api.export(["Follow"]);
+  api.export(['Follow']);
 });
